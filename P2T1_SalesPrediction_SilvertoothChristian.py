95f48c8e5b88a625f11ab65be807bb68ef166f16
D3cidu3ye <61466941+D3cidu3ye@users.noreply.github.com>
2020-02-25 10:28:21 -0500

    Add files via upload
    First submission.

---
+++ P2T1_SalesPrediction_SilvertoothChristian.py
@@ -0,0 +1,8 @@
+#
+#2/13/20
+#CTI-110 P2T1 - Sales Prediction
+#Christian Silvertooth
+#
+total_sales = float(input('Enter the projected sales: '))
+profit = total_sales * 0.23
+print('The profit is $', format(profit, ',.2f'))
